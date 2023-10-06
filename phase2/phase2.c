04ba0856ece3ef0f20ad8aaeef06945e833b9970
Ali Sartaz Khan <alisartazkhan@gmail.com>
2023-10-06 12:33:01 -0700

    create mb is good and working on send

--- phase2/phase2.c
+++ phase2/phase2.c
@@ -7,8 +7,8 @@
 #include <phase1.h>
 #include <phase2.h>
 
-#define MAX_SLOTS 2500
-#define MAX_MESSAGE 150
+// #define MAX_SLOTS 20
+// #define MAX_MESSAGE 150
 
 void phase2_clockHandler();
 int phase2_check_io();
@@ -62,25 +62,44 @@ struct Process {
 
     // phase2 fields
 
+    struct Process* nextConsumerNode;
+    struct Process* prevConsumerNode;
+    struct Process* nextProducerNode;
+    struct Process* prevProducerNode;
 
-};
 
-
-struct MB {
-  int id;
-  Message* messages;
-  int slotSize;
-  struct Process* senderQueueHead;
-  struct Process* receiverQueueHead;
 };
 
 struct Message {
-  char* payload;
-}
+  int isInitialized;
+  int messageSizeLimit;
+  void* message;
+  struct Message* nextMessage;
+  struct Message* prevMessage;
+};
 
+struct MB {
+  int isInitialized;
+  int id;
+  int mailSlotLimit;
+  int messageSizeLimit;
+  int mailSlotsAvailable;
+  struct Message* mailSlotQueueHead;
+  struct Process* consumerQueueHead;
+  struct Process* producerQueueHead;
+};
+
+// index to insert into mbTable. Increments after every mb insertion
+int mbIdCounter = 0;   
+int messageIDCounter =0;
+
+// arrays
 static struct Process shadowProcessTable[MAXPROC];
 static struct MB mbTable[MAXMBOX];
-static struct Message MessageTable[MAX_SLOTS];
+static struct Message messageSlots[MAXSLOTS];
+void (*systemCallVec[MAXSYSCALLS])(USLOSS_Sysargs*);
+
+
 
 void phase2_clockHandler(){
 
@@ -104,17 +123,85 @@ void fork(char *name, int(func)(char *), char *arg, int stacksize, int priority)
 
 // Initialize mailboxes and other data structures
 void phase2_init() {
-
+  int mbNumber = 0;
+  for (int i = 0; i< 7; i++){
+    MboxCreate(0,0);
+  }
 }
 
 void phase2_start_service_processes(){
+  // phase2_init();
+}
 
+/*
+    Description: iterates through the process table to find an open slot
+    for the created process using pidCounter % MAXPROC.
+
+    Arg: none
+
+    return int: the found slot for the process or -1 if table is full
+*/
+int findOpenMbTableSlot(){
+    int i = 0;
+    while (i<MAXMBOX){
+        int slot = mbIdCounter % MAXMBOX;
+        mbIdCounter++;
+        if (mbTable[slot].isInitialized == 0){
+            return slot;}
+        i++;
+    }
+
+    return -1;
+}
+
+int findOpenMessageSlot(){
+    int i = 0;
+    while (i<MAXSLOTS){
+        int slot = messageIDCounter % MAXSLOTS;
+        messageIDCounter++;
+        if (messageSlots[slot].isInitialized == 0){
+            return slot;}
+        i++;
+    }
+
+    return -1;
+}
+
+void printMbTable(){
+  for (int i=0;i<MAXMBOX; i++){
+    if (mbTable[i].isInitialized == 1){
+      USLOSS_Console("ID: %d | MESSAGESIZELIMIT: %d | MAILSLOTLIMIT: %d | MAILSLOTSAVAILABLE: %d\n", 
+      mbTable[i].id, mbTable[i].messageSizeLimit, mbTable[i].mailSlotLimit, mbTable[i].mailSlotsAvailable);
+    }
+  }
+}
+
+struct MB* getMb(int mbId){
+  return &mbTable[mbId%MAXMBOX];
 }
 
 // Create a new mailbox
-int MboxCreate(int slots, int slotSize) {
+int MboxCreate(int numSlots, int slotSize) {
+  if (numSlots < 0 || slotSize < 0){return -1;}
 
-  return 0;
+  struct MB m = {
+    .isInitialized = 1,
+    .id = mbIdCounter,
+    .mailSlotLimit = numSlots,
+    .messageSizeLimit = slotSize,
+    .mailSlotsAvailable = numSlots,
+    .mailSlotQueueHead = NULL,
+    .producerQueueHead = NULL,
+    .consumerQueueHead = NULL
+  };
+
+  // USLOSS_Console("%d\n", sizeof(mbTable)/ sizeof(mbTable[0]));
+  int slot = findOpenMbTableSlot();
+  if (slot == -1) {return -1;}
+
+  mbTable[slot] = m;
+
+  return mbIdCounter - 1;
 }
 
 // Release a mailbox
@@ -123,10 +210,34 @@ int MboxRelease(int mboxId) {
   return 0;
 }
 
+
 // Send message to mailbox
 int MboxSend(int mboxId, void *msgPtr, int msgSize) {
-  
+  struct MB* mb = getMb(mboxId);
+  if (msgSize != 0 && msgPtr == NULL){
+    return -1;
+  }
+  struct Message m = {
+    .isInitialized = 1,
+    .message = msgPtr,
+    .messageSizeLimit = msgSize,
+    .nextMessage = NULL,
+    .prevMessage = NULL
+  };
 
+  int slot = findOpenMessageSlot();
+  if (slot == -1){return -1;}
+
+  messageSlots[slot] = m;
+  if (mb->consumerQueueHead != NULL){ // send it to the consumer
+
+  } else { // add to the buffer
+    if (mb->mailSlotsAvailable == 0){
+      struct Process* curProc = mb->consumerQueueHead;
+      while (curProc->nextConsumerNode != NULL){curProc = curProc -> nextConsumerNode;}
+      curProc = 
+    }
+  }
   return 0;
 }
 
