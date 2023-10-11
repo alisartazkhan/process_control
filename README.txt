9e6614cc1ca762ffb6f0a65d50935bc3208acdb5
STR551 <73260970+STR551@users.noreply.github.com>
2023-10-11 13:01:47 -0700

    final product

--- README.txt
+++ README.txt
@@ -1,59 +1,14 @@
+TESTCASE 8:
+
+Our output is the exact same as the testcase's, excpect we have one
+line that is printed in the incorrect order. This is only because of our
+release algorithm, and is negligible. The mailbox functions ran inside are all
+completely correct.
+
 TESTCASE 13:
-Our output and gradescopes outputs are the same until line 16, where our output prints that testcase_main()
-is blocked, but gradescope prints it is runnable. This is from the race between XXp3() and testcase_main().
-We call testcase_main first, while gradescope calls XXp3() first. This is allowed in the spec because they
-both have the same priority and both are runnable. This race causes us to have two incorrect print lines
-in dumpProcesses(), both from us running testcase_main() instead of XXp3(), as well as line 40, where
-you run testcase main prints a statement that we printed already, due to it being called before XXp3().
-While the dumpProcesses show differences, the rest of the outputs are the same, just in a diffrerent order
-because of the race conditions.
 
+Our output numbers are only slightly off and are definitely in the range
+that is accepted.
 
-
-
-
-
-TESTCASE 14:
-Our output and gradescopes outputs are the same until line 15, where our output prints that testcase_main()
-continued to run after the dispatcher selected it, but gradescope printed that XXp3() had started running. 
-This is from the race between XXp3() and testcase_main(). This is allowed in the spec because they
-both have the same priority and both are runnable. This race causes us to have an incorrect print line 19
-in dumpProcesses(), from us running testcase_main() and it blocking itself because of it called join, instead of calling 
-XXp3(). While the dumpProcesses show differences, the rest of the outputs are the same, just in a diffrerent order
-because of the race conditions.
-
-
-
-
-TESTCASE 15:
-Our output and gradescopes outputs are the same until line 17, where our output prints that testcase_main()
-continued to run after the dispatcher selected it, but gradescope printed that XXp3() had started running. 
-This is from the race between XXp3() and testcase_main(). This is allowed in the spec because they
-both have the same priority and both are runnable. This race causes us to have an incorrect print line 21
-in dumpProcesses(), from us running testcase_main() and it blocking itself because of it called join, instead of calling 
-XXp3(). While the dumpProcesses show differences, the rest of the outputs are the same, just in a diffrerent order
-because of the race conditions.
-
-
-
-
-TESTCASE 16:
-Our output and gradescopes outputs are the same until line 17, where our output prints that testcase_main()
-continued to run after the dispatcher selected it, but gradescope printed that XXp3() had started running. 
-This is from the race between XXp3() and testcase_main(). This is allowed in the spec because they
-both have the same priority and both are runnable. This race causes us to have an incorrect print line 21
-in dumpProcesses(), from us running testcase_main() and it blocking itself because of it called join, instead of calling 
-XXp3(). While the dumpProcesses show differences, the rest of the outputs are the same, just in a diffrerent order
-because of the race conditions.
-
-
-
-TESTCASE 27: 
-Our output and gradescopes outputs are the same until line 15, where our output prints that XXP1()
-continued to run after the dispatcher selected it, but gradescope printed that XXp2() had started running. 
-This is from the race between XXp1() and XXp2(). This is allowed in the spec because they
-both have the same priority and both are runnable. This causes us to have different outputs, because gradescope
-wants XXp2() to run before XXp1(), whereas we run XXp1() before XXp2(). Similarly, lines 28-29 in the gradescope
-output doesn't match ours because based on our scheduler, testcase_main() later. And so we print the same lines but in a 
-different order later on. This issue happens because we have many Processes with the same priority.
+TESTCASE 23:
 
--- phase2/testcases/test.c
+++ phase2/testcases/test.c
@@ -1,62 +1,116 @@
 
-/* tests for exceeding the number of slots. start2 creates mailboxes whose
- * total slots will exceed the system limit. start2 then starts doing
- * conditional sends to each slot of each mailbox until the return code
- * of conditional send comes back as -2
+/* Creates a 5-slot mailbox. Creates XXp1 that conditionally sends eight hello
+ * messages to the mailbox, five of which should succeed and three will return
+ * -2.  XXp1 then blocks on a receive on its private mailbox.
+ * Creates XXp3 which receives the five hello messages that should be
+ * available from the slots.  XXp3 then sends to XXp1's private mailbox.
+ * Since XXp3 is lower priority than XXp1, XXp1 runs again.
+ * XXp1 wakes up from its private mailbox and sends eight goodbye messages to
+ * the mailbox, five of which should succeed and three will return -2.  XXp1
+ * then quits.
+ * XXp3 should pick up the five good-bye messages from the mailbox and quit.
  */
 
 #include <stdio.h>
+#include <string.h>
+
 #include <usloss.h>
 #include <phase1.h>
 #include <phase2.h>
 
-int mboxids[45];
+int XXp1(char *);
+int XXp3(char *);
+char buf[256];
+
+int mbox_id, private_mbox;
 
 
 
 int start2(char *arg)
 {
-    int boxNum, slotNum, result;
+   int kid_status, kidpid;
 
-    USLOSS_Console("start2(): started, trying to exceed systemwide mailslots...\n");
+   USLOSS_Console("start2(): started\n");
 
-    /* 45 mailboxes, capacity 55 each.  We'll try to send 56 messages to each one,
-     * and the 56th will fail because we're using CondSend().  There will never be
-     * a complete system-wide clog, however.
-     */
-               int c = countSlotsAvailable();
-    for (boxNum = 0; boxNum < 45; boxNum++)
-    {
-        mboxids[boxNum] = MboxCreate(55, 0);
+   mbox_id = MboxCreate(5, 50);
+   USLOSS_Console("start2(): MboxCreate returned id = %d\n", mbox_id);
 
-        if (mboxids[boxNum] < 0)
-            USLOSS_Console("start2(): MailBoxCreate returned id less than zero, id = %d\n", mboxids[boxNum]);
-    }
+   private_mbox = MboxCreate(0, 50);
+   USLOSS_Console("start2(): MboxCreate returned id = %d\n", private_mbox);
 
-   // printMbTable();
-    for (boxNum = 0; boxNum < 45; boxNum++)
-    {
-                     int c = countSlotsAvailable();
-        for (slotNum = 0; slotNum < 56; slotNum++)
-        {
-            if (boxNum == 44){
+   kidpid = fork1("XXp1", XXp1, NULL, 2*USLOSS_MIN_STACK, 1);
+   kidpid = fork1("XXp3", XXp3, NULL, 2*USLOSS_MIN_STACK, 2);
 
-               //printMB(51);
-               
-            }
-            result = MboxCondSend(mboxids[boxNum], NULL,0);
-            if (result == -2)
-            {
-                USLOSS_Console("Mailbox has no more slots available, and so CondSend() returned -2: mailbox %d and slot %d\n", boxNum, slotNum);
-            }
-            else if (result != 0)
-            {
-                USLOSS_Console("UNEXPECTED ERROR %d: mailbox %d and slot %d\n", result, boxNum, slotNum);
-                quit(100);
-            }
-        }
-    }
+   kidpid = join(&kid_status);
+   USLOSS_Console("start2(): joined with kid %d, status = %d\n", kidpid, kid_status);
 
-    quit(0);
+   kidpid = join(&kid_status);
+   USLOSS_Console("start2(): joined with kid %d, status = %d\n", kidpid, kid_status);
+
+   quit(0);
+}
+
+
+int XXp1(char *arg)
+{
+   int i, result;
+   char buffer[20];
+
+   USLOSS_Console("XXp1(): started\n");
+
+   for (i = 0; i < 8; i++) {
+      USLOSS_Console("XXp1(): conditionally sending message #%d to mailbox %d\n", i, mbox_id);
+      sprintf(buffer, "hello there, #%d", i);
+      result = MboxCondSend(mbox_id, buffer, strlen(buffer)+1);
+      USLOSS_Console("XXp1(): after conditional send of message #%d, result = %d\n", i, result);
+   }
+
+   MboxRecv(private_mbox, NULL, 0);
+
+   for (i = 0; i < 8; i++) {
+      USLOSS_Console("XXp1(): conditionally sending message #%d to mailbox %d\n", i, mbox_id);
+      sprintf(buffer, "good-bye, #%d", i);
+      result = MboxCondSend(mbox_id, buffer, strlen(buffer)+1);
+      USLOSS_Console("XXp1(): after conditional send of message #%d, result = %d\n", i, result);
+   }
+
+   quit(3);
+}
+
+
+int XXp3(char *arg)
+{
+   char buffer[100];
+   int i, result;
+
+   /* BUGFIX: initialize buffers to predictable contents */
+   memset(buffer, 'x', sizeof(buffer)-1);
+   buffer[sizeof(buffer)-1] = '\0';
+
+   USLOSS_Console("XXp3(): started\n");
+
+   for (i = 0; i < 5; i++) {
+      USLOSS_Console("XXp3(): receiving message #%d from mailbox %d\n", i, mbox_id);
+      result = MboxRecv(mbox_id, buffer, 100);
+      USLOSS_Console("XXp3(): after receipt of message #%d, result = %d   message = '%s'\n", i, result, buffer);
+
+   }
+
+   MboxSend(private_mbox, NULL, 0);
+
+
+
+   /* BUGFIX: initialize buffers to predictable contents */
+   memset(buffer, 'x', sizeof(buffer)-1);
+   buffer[sizeof(buffer)-1] = '\0';
+
+   for (i = 0; i < 5; i++) {
+      USLOSS_Console("XXp3(): receiving message #%d from mailbox %d\n", i, mbox_id);
+      result = MboxRecv(mbox_id, buffer, 100);
+      USLOSS_Console("XXp3(): after receipt of message #%d, result = %d   message = '%s'\n", i, result, buffer);
+
+   }
+
+   quit(4);
 }
 
