e7268096b51ff53d263e16e3ceac17880cd62909
STR551 <73260970+STR551@users.noreply.github.com>
2023-10-24 23:51:10 -0700

    Update phase3.c

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -1,3 +1,15 @@
+/**
+ * Author: Samuel Richardson and Ali Sartaz Khan
+ * Course: CSC 452
+ * Phase: 3
+ * Description: This program uses phase1 and 2 library and USLOSS to
+ * implement the syscalls for our operating system. We have all of the
+ * usermode code in phase3_usermode.c, which is what will be called by
+ * the user. Each of the functions in that file will call its 
+ * corresponding handler function in this file, which calls each
+ * function using its kernel mode functions. 
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <usloss.h>
@@ -22,6 +34,8 @@ int semCounter = 0;
   Running, and its startFunc is called. When a process has run to it's 
   entirety, its state is changed to Terminated, and after it has been
   joined, it is removed from its parent tree and its stack is freed.
+  This version of process struct is just a shadow version that only
+  keeps track of PID, func, and arg.
 */
 struct Process {
     int PID;
@@ -29,6 +43,13 @@ struct Process {
     char* arg;
 };
 
+
+/*
+  Struct that represents an a sempahore. It keeps track of value
+  to be incremented and decremented, and has a mailBoxID that is
+  the id of a mailbox of size 0 that is used as the blocking and 
+  unblocking mechanism for the sempaphor.
+*/
 struct Semaphore {
     int semID;
     int mboxID;
@@ -44,11 +65,8 @@ static struct Semaphore semaphoreTable[MAXSEMS];
     arguments passed in. Allocates the stack memory and adds
     it to the processTable array.
 
-    Arg: char *name: name of the process
     Arg: int (*startFunc)(char*): function to be run
     Arg: void *arg: argument to pass into startFunc
-    Arg: int stackSize: size of stack to allocate
-    Arg: int priority: priority of the process
 
     return int: the PID of the process
 */
@@ -65,26 +83,6 @@ int createShadowProcess(int (*func)(char*), void *arg) {
     return pidCounter-1;
 }
 
-/*
-    Description: prints information about the processTable in the same formatting
-    given by the test cases.
-
-    Arg: none
-
-    return: none
-*/
-void dumpSP(void){
-    USLOSS_Console("\n DUMPING PROC\n");
-    for (int i = 0; i < MAXPROC; i++){
-         if ( shadowProcessTable[i].PID == 0){
-             continue;
-         }
-        USLOSS_Console("PID: %d \n",shadowProcessTable[i].PID);
-
-    }
-    USLOSS_Console(" END OF DUMP PROC\n\n");
-
-}
 
 /* 
   Description: getter function that return process
@@ -94,73 +92,94 @@ void dumpSP(void){
   return: Process that you are looking for
 */
 struct Process* getProcess(int pid) {
-  //if (pid != shadowProcessTable[pid%MAXPROC].PID){createShadowProcess(pid); }  
   return &shadowProcessTable[pid%MAXPROC];
 }
 
 
 
-void printCurrentMode() {
 
-  int psr = USLOSS_PsrGet();
-
-  if (psr == USLOSS_PSR_CURRENT_INT) {
-    USLOSS_Console("Currently in USER mode\n");
-  }
-  else {
-    USLOSS_Console("Currently in KERNEL mode\n"); 
-  }
-
-}
-
+/* 
+  Description: sets the PSR of USLOSS to user mode
+   
+  Arg: None
+   
+  return: None
+*/
 void switchToUserMode(){
-
-    // Get current PSR
-int psr = USLOSS_PsrGet();  
-
-// Set user mode bit
-psr = USLOSS_PSR_CURRENT_INT;
-
-
-
-// Update PSR
-USLOSS_PsrSet(psr);
+    int psr = USLOSS_PsrGet();  
+    psr = USLOSS_PSR_CURRENT_INT;
+    USLOSS_PsrSet(psr);
 }
 
+
+/* 
+  Description: is called by each fork function passed in, and in turn
+  called the actualy function passed in from the shadow process table,
+  where the variables are stored.
+   
+  Arg: None
+   
+  return: None
+*/
 void tramp(){
+
     switchToUserMode();
     struct Process* curProcess = getProcess(getpid()); 
-        // USLOSS_Console("HERE1 PID: %d  C: %d\n", getpid(), pidCounter);
-
-    // dumpSP();
     int status = curProcess->startFunc(curProcess->arg);
-        // USLOSS_Console("HERE2\n");
-
     Terminate(status);
-
 }
 
+
+
+/* 
+  Description: we have no service processes in this project
+   
+  Arg: None
+   
+  return: None
+*/
 void phase3_start_service_processes(){
     return;
 }
 
+
+
+/* 
+  Description: the Syscall handler function for Spawn, which is kernel
+  version of Fork. The parameter's fields are cast to their specific
+  types, and after the corresponding higher level function is called,
+  the return field in arg is populated.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernSpawn(void *args){
     int (*func)(char*);
-    USLOSS_Sysargs *sysargs = args; // Cast to pointer to USLOSS_Sysargs
+    USLOSS_Sysargs *sysargs = args;
     char* name = (char*) sysargs->arg5;
     func=  sysargs->arg1;
     char* arg = (char*) sysargs->arg2;
-    int stack_size = (int)(long)sysargs->arg3; // Dereference pointer to access arg3
-    int priority = (int)(long)sysargs->arg4; // Dereference pointer to access arg3
+    int stack_size = (int)(long)sysargs->arg3; 
+    int priority = (int)(long)sysargs->arg4; 
         
-
     createShadowProcess(func,arg);
-    
     sysargs->arg1 = (void*)(long) fork1(name, tramp, arg, stack_size, priority);
 
     return 0;
 }
 
+
+/* 
+  Description: the Syscall handler function for Wait, which is kernel
+  version of Join. The parameter's fields are cast to their specific
+  types, and after the corresponding higher level function is called,
+  the return field in arg is populated.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernWait(void *args){
     USLOSS_Sysargs *sysargs = args; 
     int status;
@@ -179,19 +198,39 @@ int kernWait(void *args){
     return 0;
 }
 
+
+/* 
+  Description: the Syscall handler function for Terminate, which is kernel
+  version of Quit. The parameter's fields are cast to their specific
+  types, and after the corresponding higher level function is called,
+  the return field in arg is populated. This functino also difffers
+  from quit by looping the quit call until it is ready to be terminated.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernTerminate(void *args){
     USLOSS_Sysargs *sysargs = args; 
     int s1 = sysargs->arg1;
     int status = sysargs->arg1;
-    //USLOSS_Console("status: %d\n",status);
 
     while (join(&status) != -2){}
-    //USLOSS_Console("status: %d\n",status);
     sysargs->arg1 = status;
     quit(s1);
     return 0;
 }
 
+
+/* 
+  Description: the local  function for kernSemCreate. The
+  function creates the semaphore and adds it to the semTable.
+   
+  Arg: int id, the id of the mailbox to be used
+  Arg: int val, the val of the sem to be intialized to
+   
+  return: int -1 if errored, 0 otherwise
+*/
 int semCreate(int id, int val){
     for (int i = 0; i < MAXSEMS; i++){
         if (semaphoreTable[i].mboxID == 0){
@@ -208,34 +247,44 @@ int semCreate(int id, int val){
     return -1;
 }
 
+
+/* 
+  Description: the Syscall handler function for semCreate. The
+  function creates the semaphore and adds it to the semTable by
+  calling semCreate, and the returns the proper fields in the
+  arg function.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernSemCreate(void* args) {
     USLOSS_Sysargs *sysargs = args; 
     int value = sysargs->arg1;
     sysargs->arg4 = 0;
-    // Allocate new mailbox to use as semaphore
     int mailboxID = MboxCreate(0, 0);
-    // value might be value or 1
     if (semCreate(mailboxID, value) == -1 || value < 0){
         sysargs->arg4 = -1;
         sysargs->arg1 =0;
         return -1;
     }
 
-    // Send messages to mailbox to represent value
-    // for(int i=0; i<value; i++) {
-    //     MboxSend(mailboxID, NULL, 0); 
-    // }
     sysargs->arg1 = semCounter-1;
     return 0;
 }
 
-//lock adds one to value
-// if value greater than equaal to 0, we unblock by recving to sems mb
 
-//sory my phone died one second
-//unlock subtracts one from value
-//if value less than 0, we block the process by sennding to sem's mb
 
+/* 
+  Description: the Syscall handler function for semP. The
+  function decremens and locks the sempahor if needed,
+  using mBoxRecv and the corresponding mbox in its
+  field as the blocking mechanishm
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernSemP(void *args){
     // lock decrement
     USLOSS_Sysargs *sysargs = args; 
@@ -246,9 +295,6 @@ int kernSemP(void *args){
     sysargs->arg4 = 0;
     sem->value--;
     if (sem->value < 0){
-        // USLOSS_Console("BLOCKED!!!\n");
-        // USLOSS_Console("IN P(): SEM: %d, VAL: %d\n", sem->semID, sem->value);
-        
         int retVal = MboxRecv(mboxID, NULL, NULL);
         if (retVal == -1){
             sysargs->arg4 = -1;
@@ -257,11 +303,20 @@ int kernSemP(void *args){
         }
     }
     
-    // USLOSS_Console("IN P(): SEM: %d, VAL: %d\n", sem->semID, sem->value);
-
     return 0;
 }
 
+
+/* 
+  Description: the Syscall handler function for semV. The
+  function increments and unlocks the sempahor if needed,
+  using mBoxSend and the corresponding mbox in its
+  field as the unblocking mechanishm
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernSemV(void *args){
     // unlock increment
     USLOSS_Sysargs *sysargs = args; 
@@ -273,8 +328,6 @@ int kernSemV(void *args){
     
     sem->value++;
     if (sem->value <= 0){
-        // USLOSS_Console("UNBLOCKED!!!\n");
-        // USLOSS_Console("IN V(): SEM: %d, VAL: %d\n", sem->semID, sem->value);
 
         int retVal = MboxSend(mboxID, NULL, NULL);
         if (retVal == -1){
@@ -284,34 +337,63 @@ int kernSemV(void *args){
         }
     }
     
-    // USLOSS_Console("IN V(): SEM: %d, VAL: %d\n", sem->semID, sem->value);
 
     return 0;
 }
 
-void printSemaphore(int id){
-    struct Semaphore* s = &semaphoreTable[id];
-    USLOSS_Console("Val: %d -------------\n", s->value);
-}
 
+/* 
+  Description: the Syscall handler function for getTimeOfDay.
+  The function simply calls the curretTime function.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernGetTimeofDay(void *args){
     USLOSS_Sysargs *sysargs = args; 
     sysargs->arg1 = currentTime();
     return 0;
 }
 
+/* 
+  Description: the Syscall handler function for CPUTime.
+  The function simply calls the readTime function.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernCPUTime(void *args){
     USLOSS_Sysargs *sysargs = args; 
     sysargs->arg1 = readtime();
     return 0;
 }
 
+
+/* 
+  Description: the Syscall handler function for GetPID.
+  The function simply calls the getpid function.
+   
+  Arg: args, void pointer that holds the parameters
+   
+  return: int 0 (can be anything)
+*/
 int kernGetPID(void *args){
     USLOSS_Sysargs *sysargs = args; 
     sysargs->arg1 = getpid();
     return 0;
 }
 
+
+
+/* 
+  Description: init function that populates the systemCallVec with
+  the corresponding handler functions for each call.
+  Arg: None
+   
+  return: None
+*/
 void phase3_init(){
     systemCallVec[SYS_SPAWN] = kernSpawn;
     systemCallVec[SYS_WAIT] = kernWait;
@@ -322,7 +404,5 @@ void phase3_init(){
     systemCallVec[SYS_GETTIMEOFDAY] = kernGetTimeofDay;
     systemCallVec[SYS_GETPROCINFO] = kernCPUTime;
     systemCallVec[SYS_GETPID] = kernGetPID;
-
-
     return;
 }
