77a4ffd36658aeeae1d296e49c8970b36fb71f34
STR551 <73260970+STR551@users.noreply.github.com>
2023-11-03 20:34:18 -0700

    got the makefile to work for sams computer

--- phase4a/phase4.c
+++ phase4a/phase4.c
@@ -11,39 +11,54 @@
 #include <phase4.h>
 
 void phase4_init(void) {
-    // Initialization code for phase 4
+    systemCallVec[SYS_SLEEP] = kernSleep;
+    systemCallVec[SYS_TERMREAD] = kernTermRead;
+    systemCallVec[SYS_TERMWRITE] = kernTermWrite;
+    systemCallVec[SYS_DISKREAD] = kernDiskRead;
+    systemCallVec[SYS_DISKWRITE] = kernDiskWrite;
+    systemCallVec[SYS_DISKSIZE] = kernDiskSize;
 }
 
 void phase4_start_service_processes(){}
 
 
-int kernSleep(int seconds) {
+int kernSleep(USLOSS_Sysargs * args) {
     // Kernel mode sleep implementation
-    return 0; // Placeholder return value
+    USLOSS_Sysargs *sysargs = args; 
+    int seconds = (int) sysargs->arg1;
+    USLOSS_Console("Inside %d\n", seconds);
+
+
+    return 0;
 }
 
-int kernDiskRead(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+int kernDiskRead(void *diskBuffer, int unit, int track, int first,int sectors, int *status) {
+
     // Kernel mode disk read implementation
     return 0; // Placeholder return value
 }
 
 int kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
+
     // Kernel mode disk write implementation
     return 0; // Placeholder return value
 }
 
 int kernDiskSize(int unit, int *sector, int *track, int *disk) {
+
     // Kernel mode disk size implementation
     return 0; // Placeholder return value
 }
 
 int kernTermRead(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
+    // USLOSS_Sysargs *sysargs = args; 
+
     // Kernel mode terminal read implementation
     return 0; // Placeholder return value
 }
 
 int kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
+
     // Kernel mode terminal write implementation
     return 0; // Placeholder return value
 }
-
