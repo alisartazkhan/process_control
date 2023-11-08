f8a7370342a35e342e503c67be44ca1ca38a95ab
STR551 <73260970+STR551@users.noreply.github.com>
2023-11-08 14:59:43 -0700

    Update phase4.c

--- phase4a/phase4.c
+++ phase4a/phase4.c
@@ -1,3 +1,15 @@
+/**
+ * Author: Samuel Richardson and Ali Sartaz Khan
+ * Course: CSC 452
+ * Phase: 4a
+ * Description: This implements the device drivers for the clock and 
+ * terminals. The kernel can call sleep for a number of seconds, and 
+ * then our usermode code is called, blocking the process for the correct
+ * number of seconds. The terminal functions are also used in a driver. 
+ * We create mailboxes for each process to allow for blocking, as well 
+ * as creating daemon functions for each driver (7 in total).
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <usloss.h>
@@ -10,6 +22,8 @@
 #include <usyscall.h>
 #include <phase4.h>
 
+
+// intialize global variables
 int GLOBAL_CLOCK_TICKS = 0;
 int DAEMON_CLOCK_PID = -1;
 int DAEMON_TERMINAL0_PID = -1;
@@ -24,10 +38,13 @@ int TERM_LOCK_MBOX_ID = -1;
 int TERM_MBOX_ARRAY[4];
 
 
-
 int ourSleep(void*);
 int ourTermRead(void*);
 
+
+
+// shadow process struct that keeps track of the sleep time, next proceses 
+// in the queue, and the correponding mutex mailbox ID for blocking
 struct Process {
     int PID;
     int mboxID;
@@ -40,161 +57,109 @@ struct Process {
 static struct Process shadowProcessTable[MAXPROC];
 static struct Process * priorityQueueHead = NULL;
 
+
+/*
+    Description: initialize the syscall array valyes before the program 
+    starts running.
+
+    Param: None
+    Return: None
+
+*/
 void phase4_init(void) {
-
-
-
     systemCallVec[SYS_SLEEP] = ourSleep;
     systemCallVec[SYS_TERMREAD] = ourTermRead;
-    //systemCallVec[SYS_TERMWRITE] = ourTermWrite;
-    //systemCallVec[SYS_DISKREAD] = ourDiskRead;
-    //systemCallVec[SYS_DISKWRITE] = ourDiskWrite;
-    //systemCallVec[SYS_DISKSIZE] = ourDiskSize;
+
 }
+
 
 /*
-  Method enables interrupts using the psr.
-  
-  Args: NONE
-  Returns: void
+    Description: daemon process that handles the clock. This function has
+    an infinite loop that calls waitDevice, which waits for the clock
+    interrupt to occur, and then removes the process from the sleep queue
+
+    Param: None
+    Return: None
+
 */
-void enableInterrupts() {
-
-  unsigned int psr = USLOSS_PsrGet();
-  psr |= USLOSS_PSR_CURRENT_INT;
-  USLOSS_PsrSet(psr);
-
-}
-
-/*
-  Method disables interrupts using the psr.
-  
-  Args: NONE
-  Returns: void
-*/
-void disableInterrupts() {
-  unsigned int psr = USLOSS_PsrGet();
-  psr &= ~USLOSS_PSR_CURRENT_INT;
-  USLOSS_PsrSet(psr);
-}
-
 void daemonProcessMain(){
-    // USLOSS_Console("INSIDE THE DAEMON MAIN()\n");
     int status = -1;
     while (1) {
-        // USLOSS_Console("INSIDE THE DAEMON MAIN() louroop\n");
         waitDevice(USLOSS_CLOCK_DEV, 0, &status);
-        //USLOSS_Console("Wait Device returned ");
-        //dumpPriorityQueue();
-        //USLOSS_Console("GLOBAL CLOCK TICKS: %d\n", GLOBAL_CLOCK_TICKS);
         GLOBAL_CLOCK_TICKS ++;
-
         if (GLOBAL_CLOCK_TICKS % 10 == 0 && GLOBAL_CLOCK_TICKS < 4 * 10){
-            //USLOSS_Console("Global Clock tick: %d\n",GLOBAL_CLOCK_TICKS/10);
-            // dumpPriorityQueue();
         }
         removeFromPriorityQueue();
     }
-
-
 }
 
-void termMain(int unit){
-    int status;
-    char buffer[MAXLINE];
-    int i=0;
-    int newLine = '\n';
-    while(1) {
-        waitDevice(USLOSS_TERM_DEV, unit, &status);
-        // debug("status: %d", status);
-        int received = USLOSS_TERM_STAT_RECV(status);
-        
-        if(received) {
-            int ch =  USLOSS_TERM_STAT_CHAR(status);
-            debug("received: %d recv status: %d",  ch, received);
-            buffer[i] = (char)ch;   i++;
-            if(ch == newLine || i == MAXLINE) { // time to deliver
-                MboxSend(TERM_MBOX_ARRAY[unit], (void*)buffer, i);
-                //clear buffer
-                buffer[0] = '\0'; i =0;
-            }
-            
-            
-        }
 
-    }
-    return 1;
-}
+/*
+    Description: The terminal daemon function that handles reading and writing
+    from the terminal. THe specific terminal number is given by the unit, and then
+    the function calls an infinte loop that constantly waits for the given terminal
+    to send an interrupt. Each terminal has its own version of this function for each
+    daemon functions func and args.
+
+    Param: int unit: the unit specifying which terminal to use 
+    Return: None
+
+*/
 void termMain2(int unit){
-    // enableInterrupts();
-    // USLOSS_Console("INSIDE THE TERM MAIN() | UNIT: %d\n", unit);
-
-        //MboxRecv(TERM_LOCK_MBOX_ID, NULL, NULL); // lock
-
-    //dumpProcesses();
     int status = -1;
-
     char *buffer = (char *)calloc(MAXLINE+1, sizeof(char));
     int bufferIndex = 0;
     while (1) {
-
-        //USLOSS_Console("INSIDE THE TERM MAIN() LOOP | UNIT: %d\n", unit);
-        //USLOSS_Console("last thing to happen for this unit: %d\n",unit);
         waitDevice(USLOSS_TERM_DEV, unit, &status);
-        //USLOSS_Console("After waitDevice()\n", unit);
-
         char character = (char) USLOSS_TERM_STAT_CHAR(status);
         buffer[bufferIndex] = character;
         bufferIndex ++;
-        //USLOSS_Console("CHARS READ: %d | String: %s\n\n", bufferIndex, buffer);
-
         if (character == '\n'|| bufferIndex == MAXLINE){
-            //USLOSS_Console("about to end maybe\n");
             MboxCondSend(TERM_MBOX_ARRAY[unit], buffer, bufferIndex);
             free(buffer);
             buffer = (char *)calloc(MAXLINE, sizeof(char));
             bufferIndex = 0;
         } 
-
-        // add char received from waitDevice to buffer
-        // if buffer is now full or gets to newLine, cond send to mb
-        // reset buffer and words
-
     }
-
-        //MboxSend(TERM_LOCK_MBOX_ID, NULL, NULL); // unlock
-
-
 }
 
 
+/*
+    Description: checks to see if the enough time has elapased to remove
+    from the head of the priortiy queue. If so keep removing until the
+    new head of the queue has a time that has not hit yet, or is null
+
+    Param: None
+    Return: None
+
+*/
 void removeFromPriorityQueue(){
     int curTime = GLOBAL_CLOCK_TICKS * 100;
     while (priorityQueueHead != NULL && curTime >= priorityQueueHead->endSleep){
-        //USLOSS_Console("Inside REMOVE PQUEUE -------------------------");
-        //dumpPriorityQueue();
-
         struct Process* p = priorityQueueHead;
         priorityQueueHead = priorityQueueHead -> next;
-        //dumpPriorityQueue();
         int mboxID = p->mboxID;
         MboxRecv(mboxID, NULL, NULL);
     }
 }
 
 
+/*
+    Description: this function enables each terminal's interupts and creates
+    the 7 daemon processes, and then creates the 4 mailboxes for reading and 
+    writing to the terminal. This function is called before the dispatcher
+    starts running processes.
+
+    Param: None
+    Return: None
+
+*/
 void phase4_start_service_processes(){
-    int ctrl = 0x6; // 0 is the initial control value, you may use the current value
-    // ctrl |= 0x2;
-    // ctrl |= 0x7;
+    int ctrl = 0x6;
     assert( USLOSS_DeviceOutput(USLOSS_TERM_DEV, 0, (void*)(long)ctrl) == USLOSS_DEV_OK);
     assert( USLOSS_DeviceOutput(USLOSS_TERM_DEV, 1, (void*)(long)ctrl) == USLOSS_DEV_OK);
     assert( USLOSS_DeviceOutput(USLOSS_TERM_DEV, 2, (void*)(long)ctrl) == USLOSS_DEV_OK);
     assert( USLOSS_DeviceOutput(USLOSS_TERM_DEV, 3, (void*)(long)ctrl) == USLOSS_DEV_OK);
-
-
-
-
     DAEMON_CLOCK_PID = fork1("DAEMON", daemonProcessMain, NULL, USLOSS_MIN_STACK, 1);
     DAEMON_TERMINAL0_PID = fork1("TERM0", termMain2, 0, USLOSS_MIN_STACK, 1);
     DAEMON_TERMINAL1_PID = fork1("TERM1", termMain2, 1, USLOSS_MIN_STACK, 1);
@@ -202,23 +167,37 @@ void phase4_start_service_processes(){
     DAEMON_TERMINAL3_PID = fork1("TERM3", termMain2, 3, USLOSS_MIN_STACK, 1);
     DAEMON_DISK0_PID = fork1("DISK", NULL, NULL, USLOSS_MIN_STACK, 1);
     DAEMON_DISK1_PID = fork1("DISK", NULL, NULL, USLOSS_MIN_STACK, 1);
-
     TERM_LOCK_MBOX_ID = MboxCreate(1, 0);
-
     TERM_MBOX_ARRAY[0] = MboxCreate(10, MAXLINE);
     TERM_MBOX_ARRAY[1] = MboxCreate(10, MAXLINE);
     TERM_MBOX_ARRAY[2] = MboxCreate(10, MAXLINE);
     TERM_MBOX_ARRAY[3] = MboxCreate(10, MAXLINE);
-    
 }
 
+
+
+/*
+    Description: print a process for debugging
+
+    Param: struct Process * cur, the pointer to the process to print
+    Return: None
+
+*/
 void printProc(struct Process * cur){
 
     USLOSS_Console("[PID: %d | MBOXID: %d | ENDSLEEP %d] -->",cur->PID, cur->mboxID, cur->endSleep);
 
 }
 
-int dumpPriorityQueue(){
+
+/*
+    Description: dumps the priorty queue for debugging
+
+    Param: None
+    Return: None
+
+*/
+void dumpPriorityQueue(){
     
     struct Process * cur = priorityQueueHead;
     USLOSS_Console("\nDUMPING PQueue -------------\n\n");
@@ -231,6 +210,14 @@ int dumpPriorityQueue(){
 
 }
 
+
+/*
+    Description: dumps the shadow process table for debugging
+
+    Param: None
+    Return: None
+
+*/
 void dumpSP(void){
     USLOSS_Console("\nDUMPING SHADOW PROCTABLE ---------------------------\n\n");
     for (int i = 0; i < MAXPROC; i++){
@@ -243,57 +230,83 @@ void dumpSP(void){
     USLOSS_Console("\nEND OF DUMP PROC -----------------------\n\n");
 
 }
+
+
+/*
+    Description: creates the shadow process, adds it to the table,
+    and then returns the processes shadow pid.
+
+    Param: int es, the end time of the process
+    Param: int pid, the pid of the process
+    Return: int pid
+
+*/
 int createShadowProcess(int es, int pid) {
     struct Process p = {
         .PID = pid, 
         .mboxID = MboxCreate(0,0),
         .endSleep = es,
         .next = NULL
-
     };
     shadowProcessTable[p.PID%MAXPROC] = p;
-    // USLOSS_Console("Created new proc: PID %d | MBOXID: %d\n", p.PID, p.mboxID);
-    // dumpSP();
     return p.PID;
 }
 
-//clean up mailbox
-//update endtime
 
+/*
+    Description: our function to create the shadow process,
+    overlap any old shadow processes, and return the pid
+
+    Param: int seconds, the end time of the process
+    Param: int pid, the pid of the process
+    Return: struct Process
+
+*/
 struct Process* getProcess(int pid, int seconds) {
-    
     if (pid != shadowProcessTable[pid%MAXPROC].PID){
-        // USLOSS_Console("CREATING SHADOW PROC\n");
         createShadowProcess(seconds,pid); 
     }
     (&shadowProcessTable[pid%MAXPROC])->endSleep = seconds;
-
-
     return &shadowProcessTable[pid%MAXPROC];
 }
 
-struct Process* getProcess1(int pid) {
-    
+
+/*
+    Description: creates the shadow process, adds it to the table,
+    and then returns the processes shadow pid.
+
+    Param: int es, the end time of the process
+    Param: int pid, the pid of the process
+    Return: struct Process
+
+*/
+struct Process* getProcess1(int pid) {   
   return &shadowProcessTable[pid%MAXPROC];
 }
 
+
+
+/*
+    Description: adds the process that had just been told
+    to sleep in the correct spot in the priority queue. If 
+    it is the next process to wake up, make it the head, other
+    wise, add it to the correct spot.
+
+    Param: int pid, the pid of the process to add
+    Return: None
+
+*/
 void addToPriorityQueue(int pid){
     struct Process * p = getProcess1(pid);
-
     int priority = p->endSleep;
-
-
-    // # If the list is empty or the new node has higher priority than the current head
     if (priorityQueueHead == NULL){
         priorityQueueHead = p;
-
     }
     else if (priority <= priorityQueueHead->endSleep){
         struct Process * temp = priorityQueueHead;
         priorityQueueHead = p;
         p->next = temp;
     }
-
     else{
         struct Process * current = priorityQueueHead;
         while (current->next != NULL && current->next->endSleep <= priority){
@@ -301,101 +314,113 @@ void addToPriorityQueue(int pid){
             }
         p->next = current->next;
         current->next = p;
-        
     }
 }
 
 
+/*
+    Description: our function that gets called when Sleep gets
+    called. We created our own so that we can pass in the parameter 
+    void * that represents the syscall struct that we need. This
+    function adds the process that needs to sleep to the priorty
+    queue and then blocks the process.
+    
+    Param: void*args, the struct holding the data from the syscall
+    Return: None
+
+*/
 int ourSleep(void *args) {
-
-// dumpProcesses();
-// dumpSP();
     int startTime = GLOBAL_CLOCK_TICKS * 100;
-
-    //dumpProcesses();
-
     USLOSS_Sysargs *sysargs = args; 
     int seconds = sysargs->arg1;
-    // USLOSS_Console("Inside Our Sleep %d\n", p->endSleep);
-
-
     int endTime = seconds*1000 + startTime;
-    //USLOSS_Console("our sleep is called: startTime: %d endtime: %d seconds:%d pid:%d \n",startTime,endTime,seconds,getpid());
     struct Process * p = getProcess(getpid(), endTime);
     addToPriorityQueue(getpid());
-    // dumpPriorityQueue();
-    
-    // USLOSS_Console("BLOCKING PROC\n");
     MboxSend(p->mboxID, NULL, NULL);
-
-    // USLOSS_Console("sleep ended\n");
-
-
     return 0;
 }
 
 
-// int ourTermWrite(void* args){
-//     USLOSS_Sysargs *sysargs = args; 
+/*
+    Description: our function that gets called when termRead gets
+    called. We created our own so that we can pass in the parameter 
+    void * that represents the syscall struct that we need. This
+    function adds the process that needs to sleep to the priorty
+    queue and then blocks the process.
+    FINISH THIS
+    Param: void*args, the struct holding the data from the syscall
+    Return: None
 
-// }
-
-
+*/
 int ourTermRead(void *args){
     USLOSS_Sysargs *sysargs = args; 
     int bufferSize = sysargs->arg2;
     char *buffer = sysargs->arg1;
     int unitID = sysargs->arg3;
-    // USLOSS_Console("UNIT: %d\n", unitID);
-    // Check for invalid terminal 
     if(unitID < 0 || unitID > 3 || bufferSize <= 0 ) {
         sysargs->arg4 = -1;
         return -1;
     }
-    // MboxSend(TERM_MBOX_ARRAY[unitID], "hello there", strlen("hello there")+1);
-    
-    // memset(buffer, 'x', sizeof(bufferSize)-1);
-    // buffer[bufferSize] = '\0';
     MboxRecv(TERM_MBOX_ARRAY[unitID], buffer, bufferSize);
-    //USLOSS_Console("MESSAGE: %s\n", buffer);
-    // USLOSS_Console("STRING: %s\n", buffer);
-
     sysargs->arg2 = bufferSize;
     sysargs->arg4 = 0;   
     return 0;
 }
+
+
+
+
+
+
+/*
+Description: function for kernSleep, but is not used. Just included so the code
+compiless because this function is in the phase4a.h file.
+*/
 int kernSleep(int seconds) {
     return 0;
 }
 
 
+/*
+Description: function for kernDiskRead, but is not used. Just included so the code
+compiless because this function is in the phase4a.h file.
+*/
 int kernDiskRead(void *diskBuffer, int unit, int track, int first,int sectors, int *status) {
-
-    // Kernel mode disk read implementation
-    return 0; // Placeholder return value
+    return 0;
 }
 
+
+/*
+Description: function for kernDiskWrite, but is not used. Just included so the code
+compiless because this function is in the phase4a.h file.
+*/
 int kernDiskWrite(void *diskBuffer, int unit, int track, int first, int sectors, int *status) {
-
-    // Kernel mode disk write implementation
-    return 0; // Placeholder return value
+    return 0; 
 }
 
+
+/*
+Description: function for kernDiskSize, but is not used. Just included so the code
+compiless because this function is in the phase4a.h file.
+*/
 int kernDiskSize(int unit, int *sector, int *track, int *disk) {
-
-    // Kernel mode disk size implementation
-    return 0; // Placeholder return value
+    return 0;
 }
 
+
+/*
+Description: function for kernTermRead, but is not used. Just included so the code
+compiless because this function is in the phase4a.h file.
+*/
 int kernTermRead(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
-    // USLOSS_Sysargs *sysargs = args; 
-
-    // Kernel mode terminal read implementation
-    return 0; // Placeholder return value
+    return 0;
 }
 
-int kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
 
-    // Kernel mode terminal write implementation
-    return 0; // Placeholder return value
+/*
+Description: function for kernTermWrite, but is not used. Just included so the code
+compiless because this function is in the phase4a.h file.
+*/
+int kernTermWrite(char *buffer, int bufferSize, int unitID, int *numCharsRead) {
+    return 0;
 }
